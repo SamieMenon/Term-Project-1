b783e1a9b17800c8a7599f748026d18ba7272cbb
SamieMenon <42633818+SamieMenon@users.noreply.github.com>
2019-08-06 10:22:47 -0700

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,56 +1,89 @@
-
+/**
+ * Creates new Queue
+ * @author 
+ *
+ */
 public class Queue {
 	
-	private Room[] qList;
-	private int size, first, last;
-	private final int MAX_SIZE;
-	
-	public Queue() {
-		size = 0;
-		first = 0;
-		last = 0;
-		MAX_SIZE = 50;
-		qList = new Room[MAX_SIZE];
-	}
-	
-	public Queue(int maxSize) {
-		size = 0;
-		first = 0;
-		last = 0;
-		MAX_SIZE = maxSize;
-		qList = new Room[MAX_SIZE];
-	}
-	
-	public void enqueue(Room x) {
-		qList[last] = x;
-		last++;
-		if (last == MAX_SIZE) {
-			last = 0;
+		private final int MAX_LENGTH;
+		private Room[] queueList;
+		private int length, head, tail;
+		
+		
+		/**
+		 * Constructor function for class Queue
+		 * Creates empty Queue
+		 */
+		public Queue() {
+			length = 0;
+			head = 0;
+			tail = 0;
+			MAX_LENGTH = 50;
+			queueList = new Room[MAX_LENGTH];
 		}
-		size++;
-	}
-	
-	public Room dequeue() {
-		Room item = qList[first];
-		first++;
-		if (first == MAX_SIZE) {
-			first = 0;
+		
+		/**
+		 * Constructor function for class Queue; Creates Queue of length size
+		 * @param size, size of Queue
+		 */
+		public Queue(int size) {
+			length = 0;
+			head = 0;
+			tail = 0;
+			MAX_LENGTH = size;
+			queueList = new Room[MAX_LENGTH];
 		}
-		size--;
-		return item;
-	}
-	
-	public boolean isEmpty() {
-		return (size == 0);
-	}
-	
-	public void printQueue() {
-		for (int i = first; i < last + 1; i++) {
-			System.out.print(qList[i] + " ");
+		
+		/**
+		 * 
+		 * @param x
+		 */
+		public void enqueue(Room newRoom) {
+			queueList[tail] = newRoom;
+			tail++;
+			if (tail == MAX_LENGTH) {
+				tail = 0;
+			}
+			length++;
 		}
-		System.out.println();
-	}
+		
+		/**
+		 * Removes first room from Queue
+		 * @return new queue list
+		 */
+		public Room dequeue() {
+			Room temp = queueList[first];
+			head++;
+			if (head == MAX_LENGTH) {
+				head = 0;
+			}
+			length--;
+			return temp;
+		}
+		
+		/**
+		 * Checks if queue is empty
+		 * @return true if list is empty; else returns false
+		 */
+		public boolean isEmpty() {
+			if (length==0) {
+				return true;
+			}
+			else return false;
+		}
+		
+		/**
+		 * Prints objects in Queue
+		 */
+		public void printQueue() {
+			for (int i = head; i < tail + 1; i++) {
+				System.out.print(queueList[i] + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		
 	
-	
-	
+
 }
