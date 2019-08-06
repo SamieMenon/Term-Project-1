53849a143bfb465cfd626056a3d4ba1b6876834d
jarchan1 <51346802+jarchan1@users.noreply.github.com>
2019-08-05 22:47:58 -0700

    Changed Stack

--- Stack.java
+++ Stack.java
@@ -1,56 +1,47 @@
-
 public class Stack {
-
-	private final int size;
-	private int top, items;
-	private Room[] stList;
 	
-	public Stack() {
-		size = 50;
-		top = 0;
-		items = 0;
-		stList = new Room[size];
-	}
+	public int items, top;
+	private final int length;
+	private Room[] stck;
 	
-	public Stack(int size) {
-		this.size = size;
-		top = 0;
-		items = 0;
-		stList = new Room[size];
-	}
-	
-	public void push(Room room) {
-		if (top == size - 1) {
-			System.out.println("Stack is full");
+	// pushes to the top of the stack
+	public void push(Room rm) {
+		//if stack is full, cannot push
+		if (top == length - 1) {
+			System.out.println("Unable to push due to full stack");
+		}else {
+			stck[top] = rm;
+			items++;
+			top++;
 		}
-		stList[top] = room;
-		top++;
-		items++;
 	}
-	
+	//pops on top of stack
 	public Room pop() {
 		if (top == 0) {
-			System.out.println("Stack is empty, nothing to pop!");
+			System.out.println("Unable to pop due to empty stack");
 		}
-		Room room = stList[top - 1];
-		top--;
+		Room rm = stck[top - 1];
 		items--;
-		return room;
+		top--;
+		return rm;
 	}
-	
+	//returns true is top is empty
 	public boolean isEmpty() {
 		return (top == 0);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	//initial stack
+	public Stack() {
+		items = 0;
+		top = 0;
+		length = 100;
+		stck = new Room[length];
+	}
+	//stack with length arguments
+	public Stack(int length) {
+		items = 0;
+		top = 0;
+		this.length = length;
+		stck = new Room[length];
+	}
+
 }
